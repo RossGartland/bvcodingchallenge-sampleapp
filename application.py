691b492b57058443ef053c81f4d1b219195fc784
Ross Gartland <rgartland117@hotmail.com>
2020-10-16 22:40:59 +0100

    completed work

--- application.py
+++ application.py
@@ -6,11 +6,22 @@ from helpers.stats_helper import StatsHelper
 app = Flask(__name__)
 stats_helper = StatsHelper()
 
+
 @app.route('/')
 def homepage():
     # HINT: Pass variables through to the HTML using Flask - https://flask.palletsprojects.com/en/1.1.x/quickstart/#rendering-templates
-    return render_template('index.html', todays_date=datetime.now())
+    return render_template('index.html', todays_date=datetime.now(), ave_overall_rating=get_average_overall_rating,
+                           top_five_highest_rating=get_top_five_highest_rated,lowest_rating=get_lowest_rating() )
 
 # HINT: This could be your first statistic!
+
+
 def get_average_overall_rating():
     return stats_helper.caculate_ave_overall_rating()
+
+
+def get_top_five_highest_rated():
+    return stats_helper.top_five_highest_rated()
+
+def get_lowest_rating():
+    return stats_helper.lowest_rating()
--- helpers/stats_helper.py
+++ helpers/stats_helper.py
@@ -1,4 +1,5 @@
 from helpers.database_helper import Database
+import math
 
 
 class StatsHelper():
@@ -8,10 +9,20 @@ class StatsHelper():
         print("Stats Helping initialising!")
 
     def caculate_ave_overall_rating(self):
-        result = self.database.fetch_one("SELECT AVG(review_overall) FROM reviews")
+        result = self.database.fetch_one(
+            "SELECT ROUND(AVG(review_overall), 1) FROM reviews")
         return result[0]
 
     # HINT: You can define more queries here, along with some python logic to calculate!
     def calculate_another_stat(self):
-      # all_rows = self.database.fetch_all("")
-      return None
+        # all_rows = self.database.fetch_all("")
+        return None
+
+    def top_five_highest_rated(self):
+       result = self.database.fetch_all("SELECT beer_name, review_taste FROM reviews ORDER BY review_taste DESC LIMIT 5")
+       return result
+
+    def lowest_rating(self):
+        result = self.database.fetch_all("SELECT beer_name, review_taste FROM reviews ORDER BY (select AVG(review_overall) from reviews) ASC LIMIT 5")
+        return result
+
